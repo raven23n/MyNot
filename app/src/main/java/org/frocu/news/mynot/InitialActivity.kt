f201301deacccc98d7a52c80fcbc2402bef1d309
Fernando Rodriguez <raven23n@gmail.com>
2018-07-31 00:39:25 +0200

    Primer commit

---
+++ app/src/main/java/org/frocu/news/mynot/InitialActivity.kt
@@ -0,0 +1,12 @@
+package org.frocu.news.mynot
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class InitialActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_initial)
+    }
+}
