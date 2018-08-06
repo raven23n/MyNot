fb2d356f6472f60c3705e526ff27049694702fba
Fernando Rodríguez <raven23n@gmail.com>
2018-08-06 21:21:36 +0200

    Añadida Actividad CCAA y el NavigatorView

--- app/src/main/java/org/frocu/news/mynot/Activities/InitialActivity.kt
+++ app/src/main/java/org/frocu/news/mynot/Activities/InitialActivity.kt
@@ -3,16 +3,51 @@ package org.frocu.news.mynot.Activities
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.widget.CardView
+import android.support.v7.widget.Toolbar
+import android.view.MenuItem
+import android.view.View
 import org.frocu.news.mynot.Databases.SectionDatabase
 import org.frocu.news.mynot.Databases.SectionDatabaseFirestore
 import org.frocu.news.mynot.R
 
-class InitialActivity : AppCompatActivity() {
+class InitialActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener{
+
     var sectionDatabase: SectionDatabase = SectionDatabaseFirestore()
+    lateinit var cardViewAutonomousCommunities: CardView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_initial)
 //        sectionDatabase.readSections()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        cardViewAutonomousCommunities = findViewById(R.id.cardview_autonomous_communities)
+        cardViewAutonomousCommunities.setOnClickListener{
+            openCCAA()
+        }
+
+        val toolbar = findViewById(R.id.initial_toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+        // Navigation Drawer
+
+        var drawer_layout = findViewById<View>(R.id.drawer_layout) as DrawerLayout
+        var toggle = ActionBarDrawerToggle(this,
+                drawer_layout, toolbar, R.string.drawer_open, R.string.drawer_close)
+
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+        //val navigationView = findViewById<View>(R.id.nav_view) as NavigationView
+        val nav_view = findViewById<View>(R.id.nav_view) as NavigationView
+        nav_view.setNavigationItemSelectedListener(this)
+    }
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     fun openCCAA () {
