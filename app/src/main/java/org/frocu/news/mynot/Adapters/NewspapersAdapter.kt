a21224c078d4f5d080cbd4847f78cf408e386ece
Fernando Rodríguez <raven23n@gmail.com>
2018-08-21 12:48:20 +0200

    Hacer el RecyclerView

--- app/src/main/java/org/frocu/news/mynot/Adapters/NewspapersAdapter.kt
+++ app/src/main/java/org/frocu/news/mynot/Adapters/NewspapersAdapter.kt
@@ -1,58 +1,103 @@
 package org.frocu.news.mynot.Adapters
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.net.ConnectivityManager
+import android.support.v4.util.LruCache
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
-import com.google.firebase.firestore.*
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import com.android.volley.RequestQueue
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.ImageLoader
+import com.android.volley.toolbox.Volley
 import org.frocu.news.mynot.POJO.Newspaper
 import org.frocu.news.mynot.R
-import com.google.firebase.firestore.DocumentSnapshot
+import org.frocu.news.mynot.Singletons.NewspapersList.newspapers
 
 
 class NewspapersAdapter(
         var context : Context
 )
     : RecyclerView.Adapter<NewspapersAdapter.ViewHolder>(){
-    val TAG = "Mislugares"
-    var items : ArrayList <DocumentSnapshot> = ArrayList()
-    lateinit var registration : ListenerRegistration
-    var inflador : LayoutInflater
+
+    val requestQueue: RequestQueue
+    var inflater : LayoutInflater
     lateinit var onClickListener : View.OnClickListener
+    private var imageLoader: ImageLoader
 
     init{
-        items = ArrayList<DocumentSnapshot>()
-        inflador = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        requestQueue = Volley.newRequestQueue(context)
+        imageLoader = ImageLoader(requestQueue, object : ImageLoader.ImageCache {
+            private val cache = LruCache<String, Bitmap>(10)
+
+            override fun putBitmap(url: String, bitmap: Bitmap) {
+                cache.put(url, bitmap)
+            }
+
+            override fun getBitmap(url: String): Bitmap {
+                return cache.get(url)
+            }
+        })
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewspapersAdapter.ViewHolder {
-        val v = inflador.inflate(R.layout.individual_newspaper, null)
+        val v = inflater.inflate(R.layout.individual_newspaper, parent,false)
         v.setOnClickListener(onClickListener)
         return NewspapersAdapter.ViewHolder(v)
     }
 
-    override fun onBindViewHolder(holder: NewspapersAdapter.ViewHolder, posicion: Int) {
-        val lugar = getItem(posicion)
-//        personalizaVista(holder, lugar)
+    override fun onBindViewHolder(holder: NewspapersAdapter.ViewHolder, position: Int) {
+        var thereIsConnection: Boolean? = true
+        val objIncome = newspapers.get(position)
+        holder.newspaperName.text = objIncome.nameNewspaper
+
+        thereIsConnection = isNetworkConnected()
+        if (thereIsConnection) {
+            imageLoader.get(objIncome.imageNewspaper, object : ImageLoader.ImageListener {
+                override fun onResponse(response: ImageLoader.ImageContainer, isImmediate: Boolean) {
+                    val bitmap = response.bitmap
+                    holder.newspaperImage.setImageBitmap(bitmap)
+                    holder.newspaperImage.invalidate()
+                }
+
+                override fun onErrorResponse(error: VolleyError) {
+                    holder.newspaperImage.setImageResource(R.drawable.no_image)
+                }
+            })
+        } else {
+            holder.newspaperImage.setImageResource(R.drawable.no_image)
+        }
     }
 
     fun getItem(pos: Int): Newspaper? {
-        return items[pos].toObject(Newspaper::class.java)
+        return newspapers[pos]
     }
 
     fun getKey(pos: Int): String {
-        return items[pos].id
+        return newspapers[pos].nameNewspaper
     }
 
     override fun getItemCount(): Int {
-        return items.size
+        return newspapers.size
     }
 
     fun setOnItemClickListener(onClick: View.OnClickListener) {
         onClickListener = onClick
+    }
+
+    private fun isNetworkConnected(): Boolean {
+        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val ni = cm.activeNetworkInfo
+        return ni != null
+        /* if (ni == null) {
+                false
+           } else
+                true*/
     }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
--- app/src/main/java/org/frocu/news/mynot/POJO/NewspaperLocal.kt
+++
@@ -1,4 +0,0 @@
-package org.frocu.news.mynot.POJO
-
-class NewspaperLocal {
-}
