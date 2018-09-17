6867c9fd6c0d9e26f451a5c45968eaddcaecce2e
Fernando Rodriguez <raven23n@gmail.com>
2018-09-17 23:30:14 +0200

    Final 20180917

---
+++ app/src/main/java/org/frocu/news/mynot/Singletons/GlobalVariablesAndFuns.kt
@@ -0,0 +1,43 @@
+package org.frocu.news.mynot.Singletons
+
+import android.content.Context
+import org.frocu.news.mynot.Singletons.CCAAList.ccaaList
+import org.frocu.news.mynot.Singletons.NewsItemList.news
+import org.frocu.news.mynot.Singletons.NewspapersList.newspapers
+import org.frocu.news.mynot.Singletons.SectionList.sections
+import android.content.SharedPreferences
+import android.net.ConnectivityManager
+
+object GlobalVariablesAndFuns {
+    var positionNewspaperInCharge : Int = -1
+    var urlNewsItemActual : String = ""
+    var sectionActual : String = ""
+    var colorActual : String = "#FFEC97"
+
+    fun cleanAppArrays(){
+        sections.clear()
+        ccaaList.clear()
+        newspapers.clear()
+        news.clear()
+    }
+
+    fun checkSharedPreferencesKey(context: Context,key:String): String{
+        val preferences :SharedPreferences= context.getSharedPreferences("firstAccessVariables",Context.MODE_PRIVATE)
+        val sharedPreferencesKey = preferences.getString(key,"N")
+        return sharedPreferencesKey
+    }
+
+    fun updateSharedPreference(context: Context,key:String, value:String){
+        val preferences :SharedPreferences= context.getSharedPreferences("firstAccessVariables",Context.MODE_PRIVATE)
+        val editor = preferences.edit()
+        editor.putString(key,value)
+        editor.commit()
+    }
+
+
+    fun isNetworkConnected(context: Context): Boolean {
+        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val ni = cm.activeNetworkInfo
+        return ni != null
+    }
+}
